ec57d928abd2e5333641fa3733dea93d8dcc8362
Татьяна <tattypaw@gmail.com>
2023-02-22 22:55:22 +0800

    all

---
+++ data.js
@@ -0,0 +1,42 @@
+phones = `
+[
+    {
+    "firstName": "Тимур Иванович",
+    "lastName": "Гуев",
+    "address": {
+        "streetAddress": "Часовая 25, кв. 127",
+        "city": "Москва",
+        "postalCode": 125315
+    },
+    "phoneNumbers": ["+7 (919) 424-84-34", "+7 (916) 928-92-34"],
+    "birthday": "11.11.2001",
+    "email": "guev@mail.com",
+    "photo":"https://static.tildacdn.com/tild3538-6534-4062-a436-623665633064/16.jpg"
+},
+{
+    "firstName": "Павел Прокопьевич",
+    "lastName": "Гуров",
+    "address": {
+        "streetAddress": "Полйчасовая 25, кв. 127",
+        "city": "Москва",
+        "postalCode": 125315
+    },
+    "phoneNumbers": ["+7 (919) 424-84-33", "+7 (916) 928-92-33"],
+    "birthday": "12.11.2001",
+    "email": "gurov@mail.com",
+    "photo":"http://static.tildacdn.com/tild3734-6666-4630-b734-376432356634/0030.jpg"
+},
+{
+    "firstName": "Тамара Ивановна",
+    "lastName": "Гусева",
+    "address": {
+        "streetAddress": "Причасовая 25, кв. 127",
+        "city": "Москва",
+        "postalCode": 125315
+    },
+    "phoneNumbers": ["+7 (919) 424-84-35", "+7 (916) 928-92-35"],
+    "birthday": "11.12.2001",
+    "email": "guceva@mail.com",
+    "photo":"https://static.tildacdn.com/tild3561-3064-4366-b633-313538386530/9.jpg"
+    }
+]`
